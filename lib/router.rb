06da7be801b85e608ecf082ec4c91a36d00c68bb
joeseiden <joe.seiden214@gmail.com>
2017-02-21 18:02:28 -0800

    Complete up to 5c

--- lib/router.rb
+++ lib/router.rb
@@ -2,15 +2,18 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method, @controller_class, @action_name = pattern, http_method, controller_class, action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    !!(@pattern =~ req.path) && (@http_method == req.request_method.downcase.to_sym)
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+
   end
 end
 
@@ -18,10 +21,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -32,10 +37,17 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method (http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+    nil
   end
 
   # either throw 404 or call run on a matched route
